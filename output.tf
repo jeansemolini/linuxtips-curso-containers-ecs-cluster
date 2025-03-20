17c0e6c34598fd45c56dfe344f1aecb29fe419ef
Jean Semolini <jean.maiko@hotmail.com>
2025-03-20 11:18:30 -0300

    feat: roteamento

--- output.tf
+++ output.tf
@@ -2,10 +2,18 @@ output "load_balancer_dns" {
   value = aws_lb.main.dns_name
 }
 
+output "internal_dns" {
+  value = aws_lb.internal.dns_name
+}
+
 output "lb_ssm_arn" {
   value = aws_ssm_parameter.lb_arn.id
 }
 
 output "lb_ssm_listener_arn" {
   value = aws_ssm_parameter.lb_listener.id
+}
+
+output "cloudmap_ssm" {
+  value = aws_ssm_parameter.cloudmap.id
 }
--- parameters.tf
+++ parameters.tf
@@ -1,11 +1,29 @@
 resource "aws_ssm_parameter" "lb_arn" {
-  name  = format("/%s/ecs/lb/id", var.project_name)
+  name  = "/linuxtips/ecs/lb/id"
   type  = "String"
   value = aws_lb.main.arn
 }
 
 resource "aws_ssm_parameter" "lb_listener" {
-  name  = format("/%s/ecs/lb/listener", var.project_name)
+  name  = "/linuxtips/ecs/lb/listener"
   type  = "String"
   value = aws_lb_listener.main.id
+}
+
+resource "aws_ssm_parameter" "lb_internal_arn" {
+  name  = "/linuxtips/ecs/lb/internal/id"
+  type  = "String"
+  value = aws_lb.internal.arn
+}
+
+resource "aws_ssm_parameter" "lb_internal_listener" {
+  name  = "/linuxtips/ecs/lb/internal/listener"
+  type  = "String"
+  value = aws_lb_listener.internal.id
+}
+
+resource "aws_ssm_parameter" "cloudmap" {
+  name  = "/linuxtips/ecs/cloudmap/namespace"
+  type  = "String"
+  value = aws_service_discovery_private_dns_namespace.main.id
 }
